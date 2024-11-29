204591a444daef176dfdb6af813994274b7b2ffc
gaurav-08-patel <gauravpatelrounak@gmail.com>
2024-11-29 20:54:55 +0545

    modified app.js to generate infinte rgba colors

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-const colors=["red","green","blue","aqua","orange","rgb(156,265,176)"];
+// const colors=["red","green","blue","aqua","orange","rgb(156,265,176)"];
+
 let btn=document.querySelector(".btn");
 let colorName=document.querySelector(".color");
 
 btn.addEventListener("click", ()=>{
-    const randomNumber=getRandomNumber();
-    colorName.innerText=colors[randomNumber];
-    document.body.style.backgroundColor=colors[randomNumber];
+    const randomNumber1=getRandomNumber();
+    const randomNumber2=getRandomNumber();
+    const randomNumber3=getRandomNumber();
+     let color=`rgb(${randomNumber1},${randomNumber2},${randomNumber3})`;
+    
+    
+    colorName.innerText=color;
+    document.body.style.backgroundColor=color;
+    
 })
 
 let getRandomNumber=()=>{
-    return Math.floor(Math.random()*colors.length);
+    return Math.floor(Math.random()*260);
 }
